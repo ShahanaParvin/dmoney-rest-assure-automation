ac199ae5a06e581b7bdca561b22bf908d35bb923
unknown <Administrator@osl.local>
2024-05-07 21:43:49 +0600

    Commit rest assure in a way

---
+++ src/test/java/MyAPICalling.java
@@ -0,0 +1,76 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static io.restassured.RestAssured.given;
+
+public class MyAPICalling {
+        Properties prop;
+        MyAPICalling myAPICalling;
+        @Test
+        public void doLogin() throws ConfigurationException, IOException {
+            myAPICalling=new MyAPICalling();
+            myAPICalling.readConfig();
+            RestAssured.baseURI = myAPICalling.prop.getProperty("baseUrl");
+            Response res =
+                    given()
+                            .contentType("application/json")
+                            .body("{\n" +
+                                    "    \"email\":\"salman@roadtocareer.net\",\n" +
+                                    "    \"password\":\"1234\"\n" +
+                                    "}\n")
+                            .when()
+                            .post("/user.login")
+                            .then()
+                            .assertThat().statusCode(200).extract().response();
+
+            //   System.out.println(res.asString());
+            JsonPath jsonobj=res.jsonPath();
+            String token=jsonobj.get("token");
+            System.out.println(token);
+            saveEnvVar("token", token);
+        }
+        @Test
+        public void getUserInfo(){
+            myAPICalling=new MyAPICalling();
+            RestAssured.baseURI = myAPICalling.prop.getProperty("baseUrl");
+            Response res =
+                    given()
+                            .contentType("application/json")
+                            .header("Authorization", myAPICalling.prop.getProperty("token"))
+                            .when()
+                            .get("/user/search/id/7")
+                            .then()
+                            .assertThat().statusCode(200).extract().response();
+
+            System.out.println(res.asString());
+            JsonPath jsonobj=res.jsonPath();
+            String name=jsonobj.get("user.name");
+            System.out.println(name);
+        }
+        public static void saveEnvVar(String key, String value) throws ConfigurationException {
+            PropertiesConfiguration config=new PropertiesConfiguration("./src/test/resources/config.properties");
+            config.setProperty(key,value);
+            config.save();
+        }
+        public void readConfig() throws IOException {
+            prop=new Properties();
+            FileInputStream file=new FileInputStream("./src/test/resources/config.properties");
+            prop.load(file);
+        }
+
+        public static void main(String[] args) throws IOException {
+            MyAPICalling myAPICalling=new MyAPICalling();
+            myAPICalling.readConfig();
+            String token= myAPICalling.prop.getProperty("token");
+            System.out.println(token);
+        }
+
+}
